2fbc336397dcb2c0bf20d236e4c3cd13c9c9a7bc
mahir999 <mahirjeet@gmail.com>
2022-08-18 18:15:46 +0600

    added a loop in user class to operator output

--- User.java
+++ User.java
@@ -1,11 +1,19 @@
 package toll_Plaza;
 
+import java.util.Scanner;
+
 public class User {
    
+	 String str0="";
 	//Checking whether Is Registered Or Non Registered
 	public void checkUser()
-	{
+	{  
+		
+		
+	while(!str0.equals("NO"))
+	{	
 		Registered_User r1 = new Registered_User();
+		
 		r1.checkUser();
 		
 		if(r1.getCheck() == 0)
@@ -15,6 +23,13 @@ public class User {
 			n1.checkUser();
 			System.out.println(n1.getString());
 		}
+		
+	 System.out.println("Do You Want To Check Vehicle Number YES/NO");
+	 Scanner sc= new Scanner(System.in);
+	  str0= sc.nextLine();
+	}
+	
+	System.out.println("ThankYou For Using Toll-Managment System");
 		
 	}
 	
--- main.java
+++ main.java
@@ -6,10 +6,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		
 		User r1 = new User();
 		r1.checkUser();
+		
       
        
 
